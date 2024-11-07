4dbbc79439d9f5ff3ffa9f83445f2ee850dc4bf3
Anushya <anushya.vp19@gmail.com>
2024-11-07 12:47:00 +0530

    message

--- mysql.sh
+++ mysql.sh
@@ -39,7 +39,7 @@ validate $? "enabling mysql"
 systemctl start mysqld &>>$LOG_FILE
 validate $? "starting mysql"
 
-mysql -h mysql.hraje.online -uroot -pExpenseApp@1 
+mysql -h mysql.hraje.online -uroot -pExpenseApp@1 &>>$LOG_FILE
 if [ $? -ne 0 ]
 then 
   echo "mysql root password is not setup, setting up now" | tee -a $LOG_FILE
